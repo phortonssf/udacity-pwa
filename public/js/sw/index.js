700c5b5d653b6a1a46e696785d2455d2e899f15a
phortonssf <phorton@softstackfactory.org>
2018-01-22 07:16:32 +0000

    3.12 hijacking requests 22

--- public/js/sw/index.js
+++ public/js/sw/index.js
@@ -1,8 +1,10 @@
 self.addEventListener('fetch', function(event) {
     console.log(event.request, "Hello World");
     event.respondWith(
-        new Response('<p class="a-winner-is-me"> Hello World</p>', {
-            headers: { 'Content-Type': 'text/html' }
-        })
+        fetch('/imgs/dr-evil.gif')
+        .catch(
+            new Response('<p class="a-winner-is-me"> Hello World</p>', {
+                headers: { 'Content-Type': 'text/html' }
+            }))
     )
 });
